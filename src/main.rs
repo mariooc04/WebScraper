798817542878b3e8806c105dc320f54b58b4974e
Mario Ortega <mariooc04@gmail.com>
2024-10-29 12:44:59 +0100

    implemented function to search for directories and files tested with vectors, future implementation with dictionaries

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn scrap_web(url: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     // Parse the HTML document
     let document = Document::from(response.as_str());
-
     // Get all the links in the document
     for node in document.find(Name("a")) {
         if let Some(link) = node.attr("href") {
@@ -29,6 +28,46 @@ fn scrap_web(url: &str) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+// Function to search directories or files in a website
+// You have to pass the directories and files to search for (maybe from a dictionary)
+fn search_dir_files(url: &str, dirs: Vec<&str>, files: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    for dir in dirs {
+        let url = format!("{}/{}", url, dir);
+        let response = client.get(&url).send();
+
+        match response {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("Directory found: {}", url);
+                }
+            }
+            Err(_) => {
+                println!("Directory not found: {}", url);
+            }
+        }
+    }
+
+    for file in files {
+        let url = format!("{}/{}", url, file);
+        let response = client.get(&url).send();
+
+        match response {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("File found: {}", url);
+                }
+            }
+            Err(_) => {
+                println!("File not found: {}", url);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
     let url = "https://www.github.com/";
 
@@ -37,6 +76,16 @@ fn main() {
     }
     else {
         println!("Scraping done!");
+    }
+
+    // Test of search_dir_files function (future test with a dictionary)
+    let dirs = vec!["src", "main", "test"];
+    let files = vec!["index.html", "main.rs", "test.rs"];
+    if let Err(e) = search_dir_files(url, dirs, files) {
+        eprintln!("Error: {}", e);
+    }
+    else {
+        println!("Searching done!");
     }
 
 }
