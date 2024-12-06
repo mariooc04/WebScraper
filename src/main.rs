a4500fba723a429be78990ea30c6487ae04e96c4
Mario Ortega <mariooc04@gmail.com>
2024-12-06 12:50:57 +0100

    Starting to structure code in modules but having some problems with async blocks

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,36 @@
-use reqwest::blocking::Client;
-use select::document::Document;
-use select::predicate::Name;
-use url::Url;
+mod scrapping;
+use scrapping::scrap_web;
+use scrapping::search_dir_files;
 
-// Function to do Scrapping of a known website
-fn scrap_web(url: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let response = client.get(url).send()?.text()?;
 
-    // Parse the HTML document
-    let document = Document::from(response.as_str());
-    // Get all the links in the document
-    for node in document.find(Name("a")) {
-        if let Some(link) = node.attr("href") {
-            let url = Url::parse(link);
-            match url {
-                Ok(url) => {
-                    println!("Link: {}", url);
-                }
-                Err(_) => {
-                    println!("Invalid URL: {}", link);
-                }
-            }
+#[tokio::main]
+async fn main() {
+    let result = tokio::spawn(async {
+        let url = "https://www.github.com/";
+
+        match scrap_web(url).await {
+            Ok(data) => println!("Scrapping result: {:?}", data),
+            Err(err) => eprintln!("Error during scraping: {}", err),
         }
+
+        // Otras operaciones asincrónicas
+    })
+    .await;
+
+    if let Err(e) = result {
+        eprintln!("Error in spawned task: {:?}", e);
     }
 
-    Ok(())
-}
 
-// Function to search directories or files in a website
-// You have to pass the directories and files to search for (maybe from a dictionary)
-fn search_dir_files(url: &str, dirs: Vec<&str>, files: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-
-    for dir in dirs {
-        let url = format!("{}/{}", url, dir);
-        let response = client.get(&url).send();
-
-        match response {
-            Ok(response) => {
-                if response.status().is_success() {
-                    println!("Directory found: {}", url);
-                }
-            }
-            Err(_) => {
-                println!("Directory not found: {}", url);
-            }
-        }
-    }
-
-    for file in files {
-        let url = format!("{}/{}", url, file);
-        let response = client.get(&url).send();
-
-        match response {
-            Ok(response) => {
-                if response.status().is_success() {
-                    println!("File found: {}", url);
-                }
-            }
-            Err(_) => {
-                println!("File not found: {}", url);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn main() {
-    let url = "https://www.github.com/";
-
-    if let Err(e) = scrap_web(url) {
-        eprintln!("Error: {}", e);
-    }
-    else {
-        println!("Scraping done!");
-    }
 
     // Test of search_dir_files function (future test with a dictionary)
-    let dirs = vec!["src", "main", "test"];
+    /* let dirs = vec!["src", "main", "test"];
     let files = vec!["index.html", "main.rs", "test.rs"];
     if let Err(e) = search_dir_files(url, dirs, files) {
         eprintln!("Error: {}", e);
     }
     else {
         println!("Searching done!");
-    }
+    } */
 
 }
---
+++ src/scrapping.rs
@@ -0,0 +1,72 @@
+use std::os::unix::net::SocketAddr;
+
+use reqwest::blocking::Client;
+use select::document::Document;
+use select::predicate::Name;
+use url::Url;
+
+
+// Function to do Scrapping of a known website
+pub async fn scrap_web(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let response = client.get(url).send()?.text()?;
+
+    // Parse the HTML document
+    let document = Document::from(response.as_str());
+    // Get all the links in the document
+    for node in document.find(Name("a")) {
+        if let Some(link) = node.attr("href") {
+            let url = Url::parse(link);
+            match url {
+                Ok(url) => {
+                    println!("Link: {}", url);
+                }
+                Err(_) => {
+                    println!("Invalid URL: {}", link);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// Function to search directories or files in a website
+// You have to pass the directories and files to search for (maybe from a dictionary)
+pub async fn search_dir_files(url: &str, dirs: Vec<&str>, files: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    for dir in dirs {
+        let url = format!("{}/{}", url, dir);
+        let response = client.get(&url).send();
+
+        match response {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("Directory found: {}", url);
+                }
+            }
+            Err(_) => {
+                println!("Directory not found: {}", url);
+            }
+        }
+    }
+
+    for file in files {
+        let url = format!("{}/{}", url, file);
+        let response = client.get(&url).send();
+
+        match response {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("File found: {}", url);
+                }
+            }
+            Err(_) => {
+                println!("File not found: {}", url);
+            }
+        }
+    }
+
+    Ok(())
+}
