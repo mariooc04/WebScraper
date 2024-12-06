bef8d4bb2f3d1f59060309ac13b7dc09586c37b9
Mario Ortega <mariooc04@gmail.com>
2024-12-06 13:08:59 +0100

    Added execution in main of search_dir_files function from module scrapping

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::vec;
+
 mod scrapping;
-use scrapping::scrap_web;
-use scrapping::search_dir_files;
+
 
 
 fn main() {
     let url = "https://github.com";
 
     let _ = scrapping::scrap_web(url);
+
+    let dirs = vec!["src", "main", "test"];
+    let files = vec!["index.html", "main.rs", "test.rs"];
+
+    let _ = scrapping::search_dir_files(url, dirs, files);
 
     // Test of search_dir_files function (future test with a dictionary)
     /* let dirs = vec!["src", "main", "test"];
